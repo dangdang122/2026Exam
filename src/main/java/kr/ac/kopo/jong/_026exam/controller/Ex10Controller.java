882a66147bd3b7e3b6d331fb7d280e89a8342899
dangdang122 <vmfhxhtm122@naver.com>
2026-05-12 09:49:39 +0900

    7장 파일업로드 실습예제 2, 3

---
+++ src/main/java/kr/ac/kopo/jong/_026exam/controller/Ex10Controller.java
@@ -0,0 +1,18 @@
+package kr.ac.kopo.jong._026exam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import java.io.File;
+import java.io.IOException;
+
+@Controller
+public class Ex10Controller {
+
+
+}
